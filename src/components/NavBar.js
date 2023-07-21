17968a4eaeb30dffa575067daf35b2f3c2cf8196
Ishan Pathak <pathakishan333@gmail.com>
2023-07-21 00:28:19 -0500

    Just changed the semantics

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,10 @@ import logo from '../images/Airplane.svg'
 function NavBar() {
     return (
         <div className='nav-bar'>
-            <div className='nav-items'>
+            <nav className='nav-items'>
                 <img src={logo} alt='logo' className='svg-logo'/>
                 <h4 className='page-title'>my travel guide</h4>
-            </div>
+            </nav>
         </div>
     )
 }
